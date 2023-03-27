9dae7424f56661359fd1cd4066cc1d65b0cbb32b
Mohd Shavej <118125090+Shavejansari@users.noreply.github.com>
2023-03-27 09:26:07 +0530

    Add files via upload

---
+++ src/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+        <div class="footer">
+            27-03-2023
+        </div>
+    )
+    
+};
+
+export default Footer
---
+++ src/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <div class="header">
+            Simple React App by Mohd Shavej
+        </div>
+    )
+    
+};
+
+export default Header
